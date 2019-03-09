d894ec9144c435fb7e5a2790382e0cf9a54d2be6
dcshoecomp <jeonnlem@gmail.com>
2019-03-08 23:10:30 -0500

    Update testnoaa.py

--- testnoaa.py
+++ testnoaa.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from noaa_sdk import noaa
+CONF_ZONEID="TXZ338"
 n = noaa.NOAA()
-res = n.alerts(active=0,zone="OHZ011")
+res = n.alerts(active=0,zone=CONF_ZONEID)
 alerts = res['features']
 for alert in alerts:
     print(alert.get('properties').get('description'))
