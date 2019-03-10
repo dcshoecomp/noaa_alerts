c85e678ffe92cda409daf1745c403d8a4a48425d
dcshoecomp <jeonnlem@gmail.com>
2019-03-09 21:24:39 -0500

    Add files via upload

--- testnoaa.py
+++ testnoaa.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 from noaa_sdk import noaa
-CONF_ZONEID="TXZ338"
+CONF_ZONEID="TNZ097"
+#https://api.weather.gov/alerts?zone=TNZ097
+#https://api.weather.gov/alerts/active
 n = noaa.NOAA()
-res = n.alerts(active=0,zone=CONF_ZONEID)
+res = n.alerts(active=1,zone=CONF_ZONEID)
 alerts = res['features']
+description = alerts[0].get('properties').get('description')
+print("****description: **** ", description)
+state = alerts[0].get('properties').get('event')
+print("****event: ***** ", state)
+lengthkeys = len(alerts)
+print("****count of alerts: ****", lengthkeys)
+print("\n\n\n\n\n\n\n")
 for alert in alerts:
     print(alert.get('properties').get('description'))
