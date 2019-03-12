38f3f57f94c553e95356006472555bb2889f6426
dcshoecomp <jeonnlem@gmail.com>
2019-03-12 15:21:15 -0400

    Add files via upload

--- testnoaa.py
+++ testnoaa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from noaa_sdk import noaa
-#CONF_ZONEID="LAC121"
+CONF_ZONEID="IAZ078"
 latitude=41.477509
 longitude=-81.813951
 #place with current alert https://api.weather.gov/alerts?zone=TNZ097
@@ -16,28 +16,61 @@ except NameError:
 else:
     params={'zone': CONF_ZONEID}
     print('zoneid')
-
+params['limit'] = 3
+params={}
 print(params)
+try:
+    nws = noaa.NOAA().alerts(active=1, **params)
+    #nws = noaa.NOAA().active_alerts()
+    print(nws)
+except Exception as err:
+    print(err)
 
-nws = noaa.NOAA().alerts(active=1, **params)
 nwsalerts = nws['features']
 
-if len(nwsalerts) > 0:
-    description = nwsalerts[0].get('properties').get('description')
-    event_type = nwsalerts[0].get('properties').get('event')
-    event_urgency = nwsalerts[0].get('properties').get('urgency')
-    event_severity = nwsalerts[0].get('properties').get('severity')
+def sortedbyurgencyandseverity(prop):
+    if (prop['properties']['urgency']).lower() == 'immediate':
+        sortedvalue = 1
+    elif (prop['properties']['urgency']).lower() == 'expected':
+        sortedvalue = 10
+    elif (prop['properties']['urgency']).lower() == 'future':
+        sortedvalue = 100
+    else:
+        sortedvalue = 1000
+    if (prop['properties']['severity']).lower() == 'extreme':
+        sortedvalue = sortedvalue * 1
+    elif (prop['properties']['severity']).lower() == 'severe':
+        sortedvalue = sortedvalue * 2
+    elif (prop['properties']['severity']).lower() == 'moderate':
+        sortedvalue = sortedvalue * 3
+    else:
+        sortedvalue = sortedvalue * 4
+    return sortedvalue
+
+nwsalerts = sorted(nwsalerts, key=sortedbyurgencyandseverity)
+for d in sortedalerts: d['properties']['severity']
+
+if len(nwsalerts) > 1:
+    nwsalerts = sorted(nwsalerts, key=sortedbyurgencyandseverity)
+    for d in sortedalerts: d['properties']['severity']
+elif len(nwsalerts) == 1:
+    description = nwsalerts[0].['properties']['description']
+    event_type = nwsalerts[0].['properties']['event']
+    event_urgency = nwsalerts[0].['properties']['urgency']
+    event_severity = nwsalerts[0].['properties']['severity']
 else:
     description = 'none'
     state = 'none'
-
-print("****description: **** ", description)
-print("****event: ***** ", event_type)
-print("****urgency: ***** ", event_urgency)
-print("****severity: ***** ", event_severity)
+nwsalerts[0]['properties']['description']
+print("****description: **** ", description]
+print("****event: ***** ", event_type]
+print("****urgency: ***** ", event_urgency]
+print("****severity: ***** ", event_severity]
 lengthkeys = len(nwsalerts)
 print("****count of alerts: ****", lengthkeys)
 print("\n\n\n\n\n\n\n")
 
-#for alert in nwsalerts:
-#    print(alert.get('properties').get('description'))
+for alert in nwsalerts:
+    #print(alert.['properties']['description'])
+    (alert['properties']['description']).lower()
+any(d['properties']['urgency'] == 'Immediate' for d in nwsalerts)
