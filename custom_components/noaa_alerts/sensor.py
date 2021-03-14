728b517e431588abbcd5ea56a7d39a1ea22cec5e
dcshoecomp <16295807+dcshoecomp@users.noreply.github.com>
2021-03-13 23:15:52 -0500

    Changed Scan Interval and Manifest Version

--- custom_components/noaa_alerts/sensor.py
+++ custom_components/noaa_alerts/sensor.py
@@ -12,21 +12,22 @@ from datetime import timedelta
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.switch import (PLATFORM_SCHEMA)
-from homeassistant.const import (CONF_LATITUDE, CONF_LONGITUDE, CONF_SCAN_INTERVAL)
+from homeassistant.const import (
+    CONF_LATITUDE, CONF_LONGITUDE, CONF_SCAN_INTERVAL)
 from homeassistant.util import Throttle
 from noaa_sdk import noaa
 
 _LOGGER = logging.getLogger(__name__)
 
-__version_ = '0.0.9'
+__version_ = '21.03.0'
 
 REQUIREMENTS = ['noaa_sdk']
 
-CONF_ZONEID="zoneid"
-CONF_URGENCY="urgency"
-CONF_SEVERITY="severity"
+CONF_ZONEID = "zoneid"
+CONF_URGENCY = "urgency"
+CONF_SEVERITY = "severity"
 
-DEFAULT_ZONEID="LAT,LONG"
+DEFAULT_ZONEID = "LAT,LONG"
 
 ATTR_EVENT = 'event'
 ATTR_SEVERITY = 'severity'
@@ -34,7 +35,7 @@ ATTR_HEADLINE = 'headline'
 ATTR_INSTRUCTION = 'instruction'
 ATTR_DESCRIPTION = 'description'
 
-SCAN_INTERVAL = timedelta(seconds=60)
+DEFAULT_INTERVAL = timedelta(seconds=120)
 
 ICON = 'mdi:weather-hurricane'
 
@@ -44,8 +45,9 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_LONGITUDE): cv.longitude,
     vol.Optional(CONF_URGENCY): cv.string,
     vol.Optional(CONF_SEVERITY): cv.string,
-    vol.Optional(CONF_SCAN_INTERVAL): cv.string,
+    vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_INTERVAL): vol.All(cv.time_period, cv.positive_timedelta),
 })
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     zoneid = str(config.get(CONF_ZONEID))
@@ -53,8 +55,10 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     longitude = config.get(CONF_LONGITUDE, hass.config.longitude)
     event_urgency = str(config.get(CONF_URGENCY))
     event_severity = str(config.get(CONF_SEVERITY))
-    update_interval = config.get(CONF_SCAN_INTERVAL, SCAN_INTERVAL)
-    add_entities([noaa_alertsSensor(zoneid, event_urgency, event_severity, latitude, longitude, update_interval)], True)
+    update_interval = config.get(CONF_SCAN_INTERVAL, DEFAULT_INTERVAL)
+    add_entities([noaa_alertsSensor(zoneid, event_urgency,
+                                    event_severity, latitude, longitude, update_interval)], True)
+
 
 def sortedbyurgencyandseverity(prop):
     if (prop['urgency']).lower() == 'immediate':
@@ -75,6 +79,7 @@ def sortedbyurgencyandseverity(prop):
         sortedvalue = sortedvalue * 4
     return sortedvalue
 
+
 class noaa_alertsSensor(Entity):
     def __init__(self, zoneid, event_urgency, event_severity, latitude, longitude, interval):
         self._zoneid = zoneid
@@ -86,24 +91,26 @@ class noaa_alertsSensor(Entity):
 
     def _update(self):
         if self._zoneid != 'LAT,LONG':
-            params={'zone': self._zoneid}
+            params = {'zone': self._zoneid}
         else:
-            params={'point': '{0},{1}'.format(self.latitude,self.longitude)}
+            params = {'point': '{0},{1}'.format(self.latitude, self.longitude)}
         try:
             nws = noaa.NOAA().alerts(active=1, **params)
             nwsalerts = []
-            for alert in nws['features'] :
+            for alert in nws['features']:
                 nwsalerts.append(alert['properties'])
             self._state = len(nwsalerts)
             self._attributes = {}
-            self._attributes['alerts'] = sorted(nwsalerts, key=sortedbyurgencyandseverity)
+            self._attributes['alerts'] = sorted(
+                nwsalerts, key=sortedbyurgencyandseverity)
             self._attributes['urgency'] = self._attributes['alerts'][0]['urgency'] if self._state > 0 else None
             self._attributes['event_type'] = self._attributes['alerts'][0]['event'] if self._state > 0 else None
             self._attributes['event_severity'] = self._attributes['alerts'][0]['severity'] if self._state > 0 else None
             self._attributes['description'] = self._attributes['alerts'][0]['description'] if self._state > 0 else None
             self._attributes['headline'] = self._attributes['alerts'][0]['headline'] if self._state > 0 else None
             self._attributes['instruction'] = self._attributes['alerts'][0]['instruction'] if self._state > 0 else None
-            self._attributes['alerts_string'] = json.dumps(self._attributes['alerts'])
+            self._attributes['alerts_string'] = json.dumps(
+                self._attributes['alerts'])
         except Exception as err:
             self._state = 'Error'
             self._attributes['alerts'] = None
